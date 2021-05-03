315005d7e4a6009aa2e6a14b26750fe200d2cff1
Freeman-com <5244934@gmail.com>
2021-05-04 00:08:54 +0300

    Initial commit

---
+++ src/test/java/ru/ljs/main/LjsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ljs.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
